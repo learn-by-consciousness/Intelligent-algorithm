4c67d36259518129a8271a9f4368b2e9b101d304
learn-by-consciousness <1059397226@qq.com>
2021-07-05 20:48:15 +0800

    初版tsp

---
+++ Q1 SA_and_genetic/src/FileInfo.h
@@ -0,0 +1,23 @@
+#include "parameter.h"
+
+#ifdef DATASET0
+#define FILENAME "../tc/a280.tsp"
+#define BEST_FILENAME "../tc/a280.opt.tour"
+#define OUTFILE "../result/tcp.txt"
+#define SIZE 280
+#define PASSNUM 6
+#define ANS_PASSNUM 4
+#define INIT_RAND_TIME 100
+#endif
+
+
+
+#ifdef DATASET1
+#define FILENAME "../tc/a280.tsp"
+#define BEST_FILENAME "../tc/a280.opt.tour"
+#define OUTFILE "../result/tcp.txt"
+#define SIZE 280
+#define PASSNUM 6
+#define ANS_PASSNUM 4
+#define INIT_RAND_TIME 100
+#endif
---
+++ Q1 SA_and_genetic/src/parameter.h
@@ -0,0 +1,5 @@
+#define DATASET0
+#define ALPHA_INIT 0.98
+#define T0 10000000
+#define TN 1
+#define M 70000
